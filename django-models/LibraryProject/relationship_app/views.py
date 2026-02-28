25f73349b709383b07a1d4025362417a58c6dd5a
saybube <ebubenwamaka@gmail.com>
2026-02-28 16:39:23 +0100

    uploading from the right folder

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views.generic.detail import DetailView
+from .models import Book
+from .models import Library
 # Create your views here.
+
+def list_books(request):
+    books = Book.objects.all()  
+    context = {'list_books': books} 
+    return render(request, 'relationship_app/list_books.html', context)
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
