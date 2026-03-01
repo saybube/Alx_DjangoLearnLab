66ae6ecb5b4206330e66c7c95182ee1a489a71b3
saybube <ebubenwamaka@gmail.com>
2026-03-01 04:16:50 +0100

    updated settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -124,4 +124,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication', # Allows you to still use the browsable API
     ],
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
