197044143d2d2aa7434edbce3aa3668665d91ed4
saybube <ebubenwamaka@gmail.com>
2026-03-01 01:31:33 +0100

    modified views in bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
 # Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    """View to list all books, restricted to users with 'can_view' permission."""
+    books = Book.objects.all() 
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    pass
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    pass
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    pass
