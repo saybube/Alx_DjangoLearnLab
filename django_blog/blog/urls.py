742ffaf60c5bc566eca49559e69a45360f8854e1
saybube <ebubenwamaka@gmail.com>
2026-03-05 00:15:52 +0100

    updated urlspy

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='post-list'), # This can be your 'home'
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     # Custom registration and profile views
     path('register/', views.register, name='register'),
